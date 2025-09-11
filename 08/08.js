0375a7c443ab45203d662579854d4356682d6710
eststar <eststar1@naver.com>
2025-09-11 23:36:17 +0900

    0911_work_08

--- 08/08.js
+++ 08/08.js
@@ -1,25 +1,73 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const sels = document.querySelectorAll(".selDiv > select");
-    const inTXTs = document.querySelectorAll(".inDiv input"); 
-    const inLabels = document.querySelectorAll(".inDiv label"); 
-    
-    for(let el of sels){
-        // el.addEventListener("input", ()=>{
-        // console.log("input event");
-        // });
-        const options = el.querySelectorAll("option");
-        for(let opt of options){
-            opt.addEventListener("focus", ()=>{
-                console.log("focus");
-                // if(opt.getAttribute("value") === "℃"){
-                //     opt.toggleAttribute  setAttribute("selected");
-                // }
-            });
-            
-            
-        } 
-    
-    }
-    
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sel1 = document.querySelector("#sel1");
+    const sel2 = document.querySelector("#sel2");
+
+    const inTXT1 = document.querySelector("#txt1");
+    const inTXT2 = document.querySelector("#txt2");
+
+    const inLabel1 = document.querySelector(`[for=txt1]`);
+    const inLabel2 = document.querySelector(`[for=txt2]`);
+
+    let selVal = sel1.value;
+    let elseVal = sel2.value;
+
+    const calcTemp = () => {
+        if (inTXT1.value === '') {
+
+            inTXT2.value = '';
+            return;
+        }
+
+        let temp;
+        if (selVal === "℃")
+            temp = (parseFloat(inTXT1.value) * 9 / 5) + 32;
+        else if (selVal === "℉")
+            temp = (parseFloat(inTXT1.value) - 32) * 5 / 9;
+
+        inTXT2.value = parseFloat(temp.toFixed(4));
+    }
+
+    inTXT1.addEventListener("input", () => {
+        calcTemp();
+    });
+
+    sel1.addEventListener("change", () => {
+        selVal = sel1.value;
+
+        const options = sel2.querySelectorAll("option");
+        for (let el of options) {
+            if (el.value === selVal)
+                el.removeAttribute("selected");
+            else {
+                el.toggleAttribute("selected", true);
+                elseVal = el.getAttribute("value");
+            }
+        }
+
+        inLabel1.innerText = selVal;
+        inLabel2.innerText = elseVal;
+
+        calcTemp();
+    });
+
+    sel2.addEventListener("change", () => {
+        selVal = sel2.value;
+
+        const options = sel1.querySelectorAll("option");
+        for (let el of options) {
+            if (el.value === selVal)
+                el.removeAttribute("selected");
+            else {
+                el.toggleAttribute("selected", true);
+                elseVal = el.getAttribute("value");
+            }
+        }
+
+        inLabel2.innerText = selVal;
+        inLabel1.innerText = elseVal;
+
+        calcTemp();
+    });
 });
